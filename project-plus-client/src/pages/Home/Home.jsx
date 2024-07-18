8b650d28440648d9a72b2036738e0ac5e7cdc450
mohityede <mohityede755@gmail.com>
2024-07-18 08:19:20 +0530

    filter section created in home page

---
+++ project-plus-client/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import { Card } from "@/components/ui/card"
+import ProjectList from "../ProjectList/ProjectList"
+
+function Home() {
+
+  return (
+    <>
+      <ProjectList/>
+    </>
+  )
+}
+
+export default Home
