4a44890da9253eef43b6a9e0745c8997989eb4be
mohityede <mohityede755@gmail.com>
2024-07-18 09:31:22 +0530

    Home page UI created

---
+++ project-plus-client/src/components/Home/ProjectCard.jsx
@@ -0,0 +1,58 @@
+import { Card } from "@/components/ui/card";
+import { BadgeIcon, DividerVerticalIcon, DotFilledIcon, DotsHorizontalIcon } from "@radix-ui/react-icons";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { Badge } from "../ui/badge";
+
+const description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quis, dolorum doloribus omnis reprehenderit ipsam earum non pariatur molestiae similique! Quae inventore similique in obcaecati, ipsa dolores impedit ab pariatur!"
+const projectTags=["Java","Spring Boot","React","Javascript","MySQL","AWS","HTML"]
+
+function ProjectCard() {
+  return (
+    <>
+      <Card className="p-5 w-full lg:max-w-3xl">
+        <div className="space-y-5">
+          <div className="space-y-2">
+            <div className="flex justify-between">
+              <div className="flex items-center gap-5">
+                <h1 className="cursor-pointer font-bold text-lg font-mono">
+                  Project Name
+                </h1>
+                <DividerVerticalIcon />
+                <p className="text-sm font-thin">Fullstack</p>
+              </div>
+              <div>
+                <DropdownMenu>
+                  <DropdownMenuTrigger>
+                    <Button varient="destructive" size="custom">
+                      <DotsHorizontalIcon/>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuItem className="focus:bg-primary focus:text-white">
+                      Update
+                    </DropdownMenuItem>
+                    <DropdownMenuItem className="focus:bg-primary focus:text-white">
+                      Delete
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+            </div>
+            <p className="text-gray-500 text-sm font-thin">
+              {description.substring(0,100)}...
+            </p>
+          </div>
+          <div className="flex flex-wrap gap-2 items-center">
+            {
+              projectTags.slice(0,6).map((tag)=>
+              <Badge key={tag} className="font-normal">{tag}</Badge>)
+            }
+          </div>
+        </div>
+      </Card>
+    </>
+  );
+}
+
+export default ProjectCard;
--- project-plus-client/src/pages/ProjectList/ProjectList.jsx
+++ project-plus-client/src/pages/ProjectList/ProjectList.jsx
@@ -1,24 +1,53 @@
+import ProjectCard from "@/components/Home/ProjectCard";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { MagnifyingGlassIcon, MixerHorizontalIcon } from "@radix-ui/react-icons";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  MagnifyingGlassIcon,
+  MixerHorizontalIcon,
+} from "@radix-ui/react-icons";
 import { useState } from "react";
 
-const categories= ["All","Frontend","Backend","Fullstack","Android","Desktop","Other"]
-const tags=["HTML","CSS","Javascript","Java","Python","Golang","Kotlin","PHP","Rust"]
+const categories = [
+  "All",
+  "Frontend",
+  "Backend",
+  "Fullstack",
+  "Android",
+  "Desktop",
+  "Other",
+];
+const tags = [
+  "All",
+  "HTML",
+  "CSS",
+  "Javascript",
+  "Java",
+  "Python",
+  "Golang",
+  "Kotlin",
+  "PHP",
+  "Rust",
+];
 
 function ProjectList() {
-  const [searchKey,setSearchKey]=useState("");
-  const handleFilterChange = (element,value)=>{
-    console.log(element+ " change "+ value)
-  }
+  const [searchKey, setSearchKey] = useState("");
+  const handleFilterChange = (element, value) => {
+    console.log(element + " change " + value);
+  };
 
-  const handleSearchChange = (event)=>{
-    console.log(event.target.value)
+  const handleSearchChange = (event) => {
+    console.log(event.target.value);
     setSearchKey(event.target.value);
-  }
+  };
 
   return (
     <>
@@ -35,41 +64,51 @@ function ProjectList() {
             <CardContent className="mt-5">
               <div className="space-y-7 lg:h-[70vh]">
                 <div>
-                  <h1 className="pb-3 text-gray-500 font-medium">
-                    Category
-                  </h1>
-                  <Select 
-                  defaultValue="all" 
-                  onValueChange={(value)=>handleFilterChange("category",value)} 
-                  className="w-[100%]">
+                  <h1 className="pb-3 text-gray-500 font-medium">Category</h1>
+                  <Select
+                    defaultValue="all"
+                    onValueChange={(value) => handleFilterChange("category", value)}
+                    className="w-[100%]"
+                  >
                     <SelectTrigger>
                       <SelectValue placeholder="None" />
-                    </SelectTrigger>                 
+                    </SelectTrigger>
                     <SelectContent>
-                    {categories.map((cat,ind)=>
-                        <SelectItem className="focus:bg-[#e91919] focus:text-white" value={cat.toLowerCase()}>{cat}</SelectItem>
-                      )}
+                      {categories.map((cat, ind) => (
+                        <SelectItem
+                          className="focus:bg-primary focus:text-white"
+                          value={cat.toLowerCase()}
+                          key={ind}
+                        >
+                          {cat}
+                        </SelectItem>
+                      ))}
                     </SelectContent>
                   </Select>
                 </div>
                 <div>
-                  <h1 className="pb-3 text-gray-500 font-medium">
-                    Tags
-                  </h1>
+                  <h1 className="pb-3 text-gray-500 font-medium">Tags</h1>
                 </div>
-                <Select 
-                  defaultValue="all" 
-                  onValueChange={(value)=>handleFilterChange("tags",value)} 
-                  className="w-[100%]">
-                    <SelectTrigger>
-                      <SelectValue placeholder="None" />
-                    </SelectTrigger>                 
-                    <SelectContent>
-                      {tags.map((tag,ind)=>
-                        <SelectItem className="focus:bg-[#] focus:text-white" value={tag.toLowerCase()}>{tag}</SelectItem>
-                      )}
-                    </SelectContent>
-                  </Select>
+                <Select
+                  defaultValue="all"
+                  onValueChange={(value) => handleFilterChange("tags", value)}
+                  className="w-[100%]"
+                >
+                  <SelectTrigger>
+                    <SelectValue placeholder="None" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {tags.map((tag, ind) => (
+                      <SelectItem
+                        className="focus:bg-primary focus:text-white"
+                        value={tag.toLowerCase()}
+                        key={ind}
+                      >
+                        {tag}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
               </div>
             </CardContent>
           </Card>
@@ -77,20 +116,20 @@ function ProjectList() {
         <section className="project-list-section w-full lg:w-[48rem]">
           <div className="flex gap-2 items-center pb-5 justify-between mt-5">
             <div className="relative p-0 w-full">
-              <Input 
-              onChange={(e)=> handleSearchChange(e)} 
-              className="40% px-9 bg-white" 
-              type="text" 
-              placeholder="Search Project..."/>
-              <MagnifyingGlassIcon className="absolute top-3 left-4 text-[#e91919]"/>
+              <Input
+                onChange={(e) => handleSearchChange(e)}
+                className="40% px-9 bg-white"
+                type="text"
+                placeholder="Search Project..."
+              />
+              <MagnifyingGlassIcon className="absolute top-3 left-4 text-primary" />
             </div>
           </div>
           <div>
             <div className="space-y-5 min-h-[74vh]">
-              {
-                searchKey?[1,2,3,4].map((i)=> <div key={i}>Card{i}</div> )
-                :[5,6,7].map((i)=><div>Card{i}</div>)
-              }
+              {searchKey
+                ? [1, 2, 3, 4].map((i) => <ProjectCard key={i} />)
+                : [5, 6, 7].map((i) => <ProjectCard key={i} />)}
             </div>
           </div>
         </section>
