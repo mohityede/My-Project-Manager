f34d2e86052f65ec49c3c356341f12d12b261268
mohityede <mohityede755@gmail.com>
2024-08-01 10:49:52 +0530

    backend project setup

---
+++ project-plus-server/src/main/java/org/themohit/ProjectPlusServerApplication.java
@@ -0,0 +1,13 @@
+package org.themohit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPlusServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPlusServerApplication.class, args);
+	}
+
+}
