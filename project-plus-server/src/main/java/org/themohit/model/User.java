fd3b33e2fb40747a9ac6b3514b609ea1a6aa968f
mohityede <mohityede755@gmail.com>
2024-08-01 23:52:44 +0530

    auto password creation disabled

---
+++ project-plus-server/src/main/java/org/themohit/model/User.java
@@ -0,0 +1,25 @@
+package org.themohit.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String fullName;
+    private String email;
+    private String password;
+    private int projectSize;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "assignee",cascade = CascadeType.ALL)
+    private List<Issue> assignedIssues=new ArrayList<>();
+}
