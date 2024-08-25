412b5f848cc95e5cdfc57aad0505fcd383af412c
mohityede <mohityede755@gmail.com>
2024-08-25 19:34:26 +0530

    config changes to resolve bug

--- project-plus-server/src/main/java/org/themohit/config/ConfigConstants.java
+++ project-plus-server/src/main/java/org/themohit/config/ConfigConstants.java
@@ -1,15 +1,26 @@
 package org.themohit.config;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class ConfigConstants {
+
+    @Value("${frontend.app.url}")
+    public static String frontend;
+
+    @Value("${backend.app.url}")
+    public static String backend;
+
     public static final String SECRET_STRING="sdjfdojiojlkjdofijlijldjfoejoij48579yihk98vhlijvuvouo4";
     public static final String JWT_HEADER="Authorization";
-    public static final int JWT_EXPIRATION_MS=1000*60*60*24*30;
+    public static final long JWT_EXPIRATION_MS= 1000L *60*60*24*30;
 
     public static final List<String> CORS_ALLOW_ORIGINS= Arrays.asList(
             "http://localhost:5173/",
-            "http://localhost:3000/"
+            "http://localhost:3000/",
+            frontend,
+            backend
     );
 }
